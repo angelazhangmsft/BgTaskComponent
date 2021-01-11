a53d19bceb181a2a66b542dbc44b2b54298b2918
Angela Zhang <angzhang@microsoft.com>
2021-01-11 14:20:10 -0800

    Add project files.

---
+++ CppUWP/MainPage.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::CppUWP::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+        
+        std::wstring exampleTaskName{ L"BgTask" };
+
+        auto allTasks{ Windows::ApplicationModel::Background::BackgroundTaskRegistration::AllTasks() };
+
+        bool taskRegistered{ false };
+        for (auto const& task : allTasks)
+        {
+            if (task.Value().Name() == exampleTaskName)
+            {
+                taskRegistered = true;
+                break;
+            }
+        }
+
+        if (!taskRegistered)
+        {
+            Windows::ApplicationModel::Background::BackgroundTaskBuilder builder;
+            builder.Name(exampleTaskName);
+            builder.TaskEntryPoint(L"Net5BgTaskComponent.BgTask");
+            builder.SetTrigger(Windows::ApplicationModel::Background::SystemTrigger{
+                Windows::ApplicationModel::Background::SystemTriggerType::TimeZoneChange, false });
+            Windows::ApplicationModel::Background::BackgroundTaskRegistration task{ builder.Register() };          
+        }
+
+    }
+
+}
---
+++ CppUWP/MainPage.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::CppUWP::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::CppUWP::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
